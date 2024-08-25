b93893e0ba0593c7743f91ba0230e35a9a4e4b11
Vinicios C Soares <vinicioscsoares@gmail.com>
2024-08-25 14:19:15 -0300

    listar_servico pt1

--- ediaristas/administracao/views.py
+++ ediaristas/administracao/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from .forms import ServicoForm
+from .models import Servico
 
 # Create your views here.
 
 def cadastar_servico(request):
-    form_servico = ServicoForm()
+    if request.method=='POST':
+        form_servico = ServicoForm(request.POST)
+        if form_servico.is_valid():
+            form_servico.save()
+            return redirect('listar_servicos')
+    else:
+        form_servico = ServicoForm()
     return render(request, 'servicos/form_servico.html', {"form_servico": form_servico})
+
+def listar_servico(request):
+    servicos= Servico.objects.all()
+
+    return render(request,'servicos/lista_servico.html', {'servicos': servicos})
