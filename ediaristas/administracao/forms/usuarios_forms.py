386c4bb9e7268136408a35cd959fa1028c1fd1fa
Vinicios C Soares <vinicioscsoares@gmail.com>
2024-08-28 07:22:29 -0300

    alterando campos do cadastro de usuário

--- ediaristas/administracao/forms/usuarios_forms.py
+++ ediaristas/administracao/forms/usuarios_forms.py
@@ -8,7 +8,7 @@ class UsuarioForm(UserCreationForm):
     
     class Meta:
         model= User
-        fields={"username","email","password1","password2"}
+        fields={"username","first_name","last_name","email","password1","password2"}
         
     def save(self,commit=True):
         user=super().save(commit=False)
